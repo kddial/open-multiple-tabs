ddb99447e85a8e1eee00ae9126af4e688487fef3
Kevin Dial <kevin.dial.phone@gmail.com>
2015-11-12 02:34:58 -0500

    finished opening tabs, and number of tabs

---
+++ btn_submit.js
@@ -0,0 +1,28 @@
+
+
+
+
+alert('hello2 ');
+
+var qq = "qq";
+alert(qq);
+// qq = document.getElementById("input_url").value;
+
+alert(qq);
+// alert(qq);
+// };
+
+
+
+
+
+
+// function myFunction() {
+//     // alert();
+//     // alert(document.getElementById("input_url").value);
+
+//     qq = document.getElementById("input_url").value;
+//     alert(qq);
+// }
+
+// myFunction()
--- popup.js
+++ popup.js
@@ -1,17 +1,83 @@
 
 /* add event listeners to buttons */
-document.getElementById('btn_submit').addEventListener('click', hello);
+document.getElementById('btn_open_tabs').addEventListener('click', call_btn_open_tabs);
 
-function hello() {
-  chrome.tabs.executeScript({
-    file: 'alert.js'
-  }); 
+/* Get url input */
+function get_input_url() {
+  var url = document.getElementById("input_url").value;
+
+  if (url.length == 0) {
+    /* Check if url exist */
+    set_error("Please input url");
+    return;
+
+  } else if (url.substring(0, 4) != "http") {
+    /* Check if url begins with 'http' */
+    set_error("URL must begin with 'http'. <br> (i.e.'https://www.google.ca/')");
+    return;
+
+  }else {
+    /* All checking pass. Remove error message */
+    remove_error();
+    return url;
+  }
 }
 
+/* Get number of tabs */
+function get_input_n_tabs() {
+  var n_tabs = document.getElementById("input_n_tabs").value;
 
+  if (!n_tabs) {
+    // Check if url exist
+    set_error("Please input a number");
+    console.log("dafuq");
+    return;
 
+  } else if (n_tabs > 10) {
+    // Check if url begins with 'http' 
+    set_error("Please do not exceed 10 tabs");
+    console.log("dafuq2");
+    return;
 
-// <button onclick="myFunction()">Try it</button>
-// function myFunction() {
-//     document.getElementById("myText").value = "Johnny Bravo";
-// }
+  }else {
+    // All checking pass. Remove error message
+    remove_error();
+    console.log("dafuq guuci");
+    return n_tabs;
+  }
+}
+
+/* Open multiple tabs */
+function call_btn_open_tabs() {
+  var i;
+  var url;
+  var n_tabs;
+
+  // Exit when url error 
+  url = get_input_url();
+  if (!url) {
+    return;
+  }
+
+  // Exit when n_tabs error
+  n_tabs = get_input_n_tabs();
+  if (!n_tabs) {
+    return;
+  }
+
+  // open multiple tabs
+  for (i = 0; i < n_tabs; i++) { 
+    chrome.tabs.create({url: url});
+  }
+}
+
+/* Set error message */
+function set_error(msg) {
+  document.getElementById("error_msg").innerHTML = msg;
+}
+
+/* Remove error message */
+function remove_error() {
+  document.getElementById("error_msg").innerHTML = "";
+}
+
